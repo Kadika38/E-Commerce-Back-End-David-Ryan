c785a0b43a77b365f984bc60426f0a84087bab4b
Kadika <dryan10101@gmail.com>
2022-06-24 16:25:12 -0700

    Set up get all and get by id routes for product

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   try {
     const allCatData = await Category.findAll({
-      include: [{ model: Product}],
+      include: [{ model: Product }],
     });
     res.status(200).json(allCatData);
   } catch (err) {
